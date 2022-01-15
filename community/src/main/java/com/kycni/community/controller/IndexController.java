bcd88ef51b778fd651abcb9a2f93d2899542c0cc
kycni <syq278872032@gmail.com>
2022-01-15 17:56:11 +0800

    add index and bootstrap

---
+++ community/src/main/java/com/kycni/community/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.kycni.community.controller;
+
+/**
+ * @author Kycni
+ * @date 2022/1/15 17:48
+ */
+public class IndexController {
+    public String index () {
+        return "/";
+    }
+}
